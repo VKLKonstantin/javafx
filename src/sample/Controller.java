b39d96d95f768ddd1b0b2e61321aa629ec8b91af
Konstantin <kos8533@yandex.ru>
2020-11-25 22:12:40 +0300

    part2_lesson4

---
+++ src/sample/Controller.java
@@ -0,0 +1,36 @@
+package sample;
+
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.scene.control.TextField;
+
+
+public class Controller {
+    @FXML
+    private TextField inputField;
+
+    @FXML
+    private ListView listView;
+
+    private final ObservableList<String> wordList = FXCollections.observableArrayList("привет", "привет2");
+
+    @FXML
+    public void initialize() {
+        listView.setItems(wordList);
+    }
+
+    @FXML
+    public void addWordToList() {
+        String word = inputField.getText();
+        if (word.isBlank()) {
+            System.out.println("Пустота");
+            return;
+        }
+        listView.getItems().add(word);
+        inputField.clear();
+    }
+
+}
